655b52548d4cb8e00e8bdd2a9ad002dc5f84ae6a
Eric Lodi Luz <eric_lodiluz@hotmail.com>
2023-12-01 00:34:59 -0300

    updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2 as cv
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import os
+import matplotlib.pyplot as plt
 
 Tk().withdraw()
 load_image = askopenfilename()
@@ -22,12 +23,14 @@ def encode_faces(folder):
 
     return list_people_encoding
 
-def find_target_face():
+def find_target_faces():
     face_locations = fr.face_locations(target_image)
 
     if not face_locations:
-        print("No face found in the target image.")
-        return
+        print("Nenhuma pessoa foi identificada.")
+        return None
+
+    target_faces = []
 
     for person in encode_faces('people/'):
         encoded_face, filename = person
@@ -36,20 +39,31 @@ def find_target_face():
 
         for i, location in enumerate(face_locations):
             if is_target_face[i]:
-                label = filename
-                create_frame(location, label)
+                label = os.path.splitext(filename)[0]
+                target_faces.append((label, location))
 
-def create_frame(location, label):
-    top, right, bottom, left = location
+    if not target_faces:
+        print("Nenhuma pessoa foi identificada.")
+        return None
 
-    cv.rectangle(target_image, (left, top), (right, bottom), (255, 0, 0), 2)
-    cv.rectangle(target_image, (left, bottom + 20), (right, bottom), (255, 0, 0), cv.FILLED)
-    cv.putText(target_image, label, (left + 3, bottom + 14), cv.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1)
+    return target_faces
 
-def render_image():
-    rgb_img = cv.cvtColor(target_image, cv.COLOR_BGR2RGB)
-    cv.imshow('Face Recognition', rgb_img)
-    cv.waitKey(0)
+def create_frames(image, faces):
+    for label, location in faces:
+        top, right, bottom, left = location
 
-find_target_face()
-render_image()
+        cv.rectangle(image, (left, top), (right, bottom), (255, 0, 0), 2)
+        cv.rectangle(image, (left, bottom + 20), (right, bottom), (255, 0, 0), cv.FILLED)
+        cv.putText(image, label, (left + 3, bottom + 14), cv.FONT_HERSHEY_DUPLEX, 0.4, (255, 255, 255), 1)
+
+def render_images(target_faces):
+    if target_faces:
+        create_frames(target_image, target_faces)
+
+        plt.imshow(target_image)
+        plt.title('Pessoas identificadas')
+        plt.axis('off')
+        plt.show()
+
+target_faces = find_target_faces()
+render_images(target_faces)
